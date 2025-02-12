b8f470009b42f90a211a27d9af724f7922f319d0
NitinBot001 <112721096+NitinBot001@users.noreply.github.com>
2025-02-12 12:17:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ ytmusic = YTMusic()
 def get_related_songs():
     video_id = request.args.get('video_id')
     if not video_id:
-        return jsonify({'error': 'Please provide a video ID'}), 400
-    
+        return jsonify({'error': 'Please provide a video_id'}), 400
+
     # Get related songs
     related_songs = ytmusic.get_watch_playlist(videoId=video_id)['tracks']
-    
+
     # Prepare response
     result = []
     for song in related_songs:
@@ -22,9 +22,9 @@ def get_related_songs():
             'title': song['title'],
             'videoId': song['videoId'],
             'artists': ', '.join(artist['name'] for artist in song['artists']),
-            'length' : song['length'] || 'undifined',
+            'length' : song['length'] or 'undifined',
         })
-    
+
     return jsonify(result)
 
 @app.route('/search_songs', methods=['GET'])
@@ -32,22 +32,25 @@ def search_songs():
     query = request.args.get('query')
     if not query:
         return jsonify({'error': 'Please provide a search query'}), 400
-    
+
     # Perform search
     search_results = ytmusic.search(query, filter='songs')
-    
+
     # Prepare response
+    print(search_results)
     result = []
     for song in search_results:
         result.append({
             'title': song['title'],
             'videoId': song['videoId'],
             'artists': ', '.join(artist['name'] for artist in song['artists']),
-            'duration' : song['duration'] || 'undifined',
-            'views' : song['views'] || 'undifined'
+            'duration' : song['duration'] or 'undifined'
+            # 'views' : song['views'] or 'undifined'
         })
-    
+
     return jsonify(result)
 
 if __name__ == '__main__':
     app.run(port=8000, host='0.0.0.0', debug=True)
+
+            
